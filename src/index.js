f848cb0104fb092fbcb39188b6284cce1173cc8f
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-04-20 14:09:24 +0530

    refactor : deleted default files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { StrictMode } from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
