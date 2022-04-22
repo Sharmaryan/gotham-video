e145c08e663f57ac05cadcb5b8dfef7f949c5fee
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-04-22 11:58:17 +0530

    installed : react icons and react router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-
+import { BrowserRouter as Router } from "react-router-dom";
 // Call make Server
 makeServer();
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>
 );
