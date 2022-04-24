a4de32fa13484036053e32ad5818cc3ac1951039
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-04-24 09:21:44 +0530

    feat : added basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { Navbar } from "./components/index";
 import { Routes, Route } from "react-router-dom";
+import {Home, History, LikedVideos, Playlists, WatchLater,Explore} from './pages/index';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/explore' element={<Explore/>}/>
+        <Route path='/history' element={<History/>}/>
+        <Route path='/liked-videos' element={<LikedVideos/>}/>
+        <Route path='/playlists' element={<Playlists/>}/>
+        <Route path='/watch-later' element={<WatchLater/>}/>
       </Routes>
     </div>
   );
---
+++ src/pages/LikedVideos/LikedVideos.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LikedVideos = () => {
+  return (
+    <div>LikedVideos</div>
+  )
+}
