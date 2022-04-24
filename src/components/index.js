c57d70f1f307ef5af4e77796b861904eabc28eb5
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-04-24 13:30:31 +0530

    feat : added explore videos

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export {Navbar} from './Navbar/Navbar';
+export {ExploreVideos} from './ExploreVideos/ExploreVideos';
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+import './Home.css';
+import { ExploreVideos } from '../../components/index';
 export const Home = () => {
-  return <div> Home</div>;
+  return <div className="home"> 
+  <ExploreVideos/>
+  </div>;
 };
