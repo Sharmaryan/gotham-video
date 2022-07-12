a973f489e77095a4a656f874a608dd1c65d2b4f3
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-12 17:53:04 +0530

    removed : unused files

--- src/context/index.js
+++ src/context/index.js
@@ -1,7 +1,1 @@
-export {AuthProvider, useAuth} from './auth-context/auth-context';
 export {VideoProvider, useVideo} from './video-context/video-context';
-export {LikeProvider, useLike} from './like-context/like-context';
-export {WatchLaterProvider, useWatchLater} from './watch-context/watch-context';
-export {HistoryProvider, useHistory} from './history-context/history-context';
-export {PlaylistProvider, usePlayList} from './playlist-context/playlist-context';
-export {SinglePlaylistProvider, useSinglePlayList} from './single-playlist-context/single-playlist-context';
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,2 @@
-export { likeVideo, unLikeVideo, removeLikedVideo } from "./likeServices";
-export { removeFromWatchLater, addToWatchLater, removeWatchLater } from "./watchLater";
-export { addVideoToHistory, clearAllHistory, removeFromHistory } from "./history";
 export { category } from "./category";
-export {
-  addAndDeleteFromPlaylist,
-  createPlaylist,
-  removeFromPlaylist, clearSinglePlaylist
-} from "./playlist";
+
