9f267b246bce1f1dea858e0a1f9149ac658f596c
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-31 19:18:07 +0530

    styles : like videos

--- src/pages/LikedVideos/LikedVideos.js
+++ src/pages/LikedVideos/LikedVideos.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { VideoLike, Sidebar } from "components";
+import { VideoLike, Sidebar, MobileSidebar } from "components";
 import "./LikedVideos.css";
 export const LikedVideos = () => {
   return (
     <div className="liked-videos">
       <Sidebar />
       <VideoLike />
+      <MobileSidebar/>
     </div>
   );
 };
