3074a76b3f8b65f28a2064e467d7d18d64771534
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-31 19:20:08 +0530

    styles : home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Home.css";
-import { ExploreVideos, Categories, Social } from "../../components/index";
+import { ExploreVideos, Categories, Social, MobileSidebar } from "../../components/index";
 export const Home = () => {
   return (
     <div className="home">
       <ExploreVideos />
       <Categories />
       <Social />
+      <MobileSidebar/>
     </div>
   );
 };
